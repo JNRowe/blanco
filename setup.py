cc59b4401af8541e080ecb11f83e7e404fdb7566
James Rowe <jnrowe@gmail.com>
2014-01-28 01:04:44 +0000

    Bump copyright years

--- setup.py
+++ setup.py
@@ -1,6 +1,7 @@
 #! /usr/bin/python -tt
+# coding=utf-8
 """setup - distutils info for blanco"""
-# Copyright (C) 2010-2012  James Rowe <jnrowe@gmail.com>
+# Copyright © 2010-2014  James Rowe <jnrowe@gmail.com>
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
