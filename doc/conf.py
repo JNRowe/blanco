67b73baf11de12fc4b2f6a7e77c31dd0abbca62d
James Rowe <jnrowe@gmail.com>
2014-01-28 03:38:11 +0000

    Bump copyright years in Sphinx config

--- doc/conf.py
+++ doc/conf.py
@@ -42,7 +42,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'blanco'
-copyright = u'2010-2012, James Rowe'
+copyright = u'2010-2014, James Rowe'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
