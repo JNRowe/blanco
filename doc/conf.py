168f9a5062c0a5cb8c69e5cf34c385f09e3d0a34
James Rowe <jnrowe@gmail.com>
2017-12-13 11:44:53 +0000

    Merge now standard Sphinx config changes

--- doc/conf.py
+++ doc/conf.py
@@ -1,5 +1,5 @@
 #
-"""conf - Sphinx configuration information"""
+"""conf - Sphinx configuration information."""
 # Copyright © 2010-2014  James Rowe <jnrowe@gmail.com>
 #
 # This program is free software: you can redistribute it and/or modify
@@ -21,45 +21,55 @@ import sys
 
 from subprocess import (CalledProcessError, check_output)
 
-root_dir = os.path.dirname(os.path.dirname(os.path.realpath(__file__)))
+root_dir = os.path.dirname(os.path.dirname(__file__))
 sys.path.insert(0, root_dir)
 
-import blanco
+import blanco  # NOQA: E402
+
+on_rtd = os.getenv('READTHEDOCS')
+if not on_rtd:
+    import sphinx_rtd_theme
 
 extensions = \
-    ['sphinx.ext.%s' % ext for ext in ['autodoc', 'coverage', 'doctest',
-                                       'intersphinx', 'viewcode']] \
-    + ['sphinxcontrib.%s' % ext for ext in []]
+    ['sphinx.ext.{}'.format(ext)
+     for ext in ['autodoc', 'coverage', 'doctest', 'intersphinx', 'napoleon',
+                 'todo', 'viewcode']] \
+    + ['sphinxcontrib.{}'.format(ext) for ext in []]
 
-# Only activate spelling, if it is installed.  It is not required in the
-# general case and we don't have the granularity to describe this in a clean
-# way
-try:
-    from sphinxcontrib import spelling  # NOQA
-except ImportError:
-    pass
-else:
-    extensions.append('sphinxcontrib.spelling')
+
+if not on_rtd:
+    # Only activate spelling if it is installed.  It is not required in the
+    # general case and we don’t have the granularity to describe this in a
+    # clean way
+    try:
+        from sphinxcontrib import spelling  # NOQA: E401
+    except ImportError:
+        pass
+    else:
+        extensions.append('sphinxcontrib.spelling')
 
 master_doc = 'index'
 source_suffix = '.rst'
 
 project = u'blanco'
-copyright = blanco.__copyright__
+copyright = '2010-2017  James Rowe'
 
-version = '.'.join(blanco.__version__.split('.')[:2])
-release = blanco.__version__
+version = '.'.join([str(s) for s in blanco._version.tuple[:2]])
+release = blanco._version.dotted
+
+html_experimental_html5_writer = True
+
+# readthedocs.org handles this setup for their builds, but it is nice to see
+# approximately correct builds on the local system too
+if not on_rtd:
+    html_theme = "sphinx_rtd_theme"
+    html_theme_path = [sphinx_rtd_theme.get_html_theme_path(), ]
 
 pygments_style = 'sphinx'
-html_theme_options = {
-    'externalrefs': True,
-}
-try:
-    html_last_updated_fmt = check_output(['git', 'log',
-                                          "--pretty=format:'%ad [%h]'",
-                                          '--date=short', '-n1'])
-except CalledProcessError:
-    pass
+with suppress(CalledProcessError):
+    proc = check_output(['git', 'log', "--pretty=format:'%ad [%h]'",
+                         '--date=short','-n1'])
+    html_last_updated_fmt = proc.stdout.decode()
 
 man_pages = [
     ('blanco.1', 'blanco', u'blanco Documentation', [u'James Rowe'], 1)
@@ -69,10 +79,21 @@ man_pages = [
 autoclass_content = 'init'
 autodoc_default_flags = ['members', ]
 
-default_role = 'py:obj'
+# intersphinx extension settings
 intersphinx_mapping = {
-    'python': ('http://docs.python.org/', os.getenv('SPHINX_PYTHON_OBJECTS')),
-}
+    k: (v, os.getenv('SPHINX_{}_OBJECTS'.format(k.upper())))
+    for k, v in {
+        'click': 'http://click.pocoo.org/6/',
+        'jnrbase': 'http://jnrbase.readthedocs.io/en/latest/',
+        'python': 'https://docs.python.org/3/',
+}.items()}
 
+# spelling extension settings
 spelling_lang = 'en_GB'
 spelling_word_list_filename = 'wordlist.txt'
+
+# napoleon extension settings
+napoleon_numpy_docstring = False
+
+# todo extension settings
+todo_include_todos = True
